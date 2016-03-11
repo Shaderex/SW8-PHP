e58f4bb81105e10c7c3716609af56c9fe82a88d6
Rasmus Holm Jensen <rhje12@student.aau.dk>
2016-03-11 10:42:23 +0100

    Add simple test to ParticipantTest

--- tests/ParticipantTest.php
+++ tests/ParticipantTest.php
@@ -1,10 +1,16 @@
 <?php
 
+use DataCollection\Participant;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ParticipantTest extends TestCase
 {
-
+    public function testParticipant()
+    {
+        $participant = new Participant;
+        $participant->id = 1;
+        $participant->deviceID = "SOME_STRING";
+    }
 }
